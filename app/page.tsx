42d9612ecbc88f7c7acada5b7f7abe432d7a23e1
DavidHODs <davidoluwatobi41@gmail.com>
2025-11-17 10:03:53 +0100

    setup and home component

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-row items-stretch">
+      <div className="flex flex-col items-center justify-center w-full h-full text-center p-8">
+        <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">
+          Notion-Lite
+        </h1>
+        <p className="text-lg text-gray-600 dark:text-gray-300">
+        Setup
+      </p>
+      </div>
+    </main>
+  )
+}
